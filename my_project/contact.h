bc830427c2e316f31b6c3aba8c8e92052c9bd55d
yinyinyin <yinxi251221@gmail.com>
2025-07-20 14:47:28 +0800

    Update contact.h

--- my_project/contact.h
+++ my_project/contact.h
@@ -2,32 +2,39 @@
 #define CONTACT_H
 
 #include<QString>
-
+#include<QJsonObject>
 
 class contact
 {
 private:
+    //基本信息
     QString name;
     QString number;
     QString group;
     QString email;
-    
-    
+
 public:
     //构造函数
     contact();
-    contact(const QString&name,const QString&number,const QString&group="",const QString&email="");
+    contact(const QString& name, const QString& number, const QString& group = "", const QString& email = "");
+
     //修改信息
-    void setname(QString&name);
-    void setnumber(QString&number);
-    void setgroup(QString&group);
-    void setemail(QString&email);
+    void setname(const QString& name);
+    void setnumber(const QString& number);
+    void setgroup(const QString& group);
+    void setemail(const QString& email);
+
     //获取信息
     QString getname();
     QString getnumber();
     QString getgroup();
     QString getemail();
-    
+
+    // 序列化成 QJsonObject
+    QJsonObject toJson() const;
+
+    // 从 QJsonObject 创建 contact
+    static contact fromJson(const QJsonObject &obj);
 };
 
 #endif // CONTACT_H
