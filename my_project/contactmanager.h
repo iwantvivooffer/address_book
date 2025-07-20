faa2f8f1de3431646ccb91564a37bf1c2386c06e
yinyinyin <yinxi251221@gmail.com>
2025-07-20 14:48:49 +0800

    Update contactmanager.h

--- my_project/contactmanager.h
+++ my_project/contactmanager.h
@@ -4,26 +4,30 @@
 #include<QList>
 #include<QString>
 #include<QSettings>
+#include<QJsonArray>
+
 #include"contact.h"
 
 class contactManager
 {
 private:
-    QList<contact>contacts;
+    QList<contact> contacts;
     
 public:
     contactManager();
     //增删联系人
     void addcontact(contact&contact);
     bool deletecontact(QString&name);
+
     //获取联系人们
     QList<contact> getcontacts();
     QList<contact> getcontactsByGroup(QString&group);
-    // 添加保存和加载函数
-    void saveContacts();
-    void loadContacts();
-    
-    
+
+    //保存联系人到 JSON 文件
+    void saveToJson(const QString &filename);
+    //从 JSON 文件加载联系人
+    void loadFromJson(const QString &filename);
+
 };
 
 #endif // CONTACTMANAGER_H
