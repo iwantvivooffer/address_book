0855adf1785e39a27f16ab78b70f6b597f9502b8
yinyinyin <yinxi251221@gmail.com>
2025-07-23 15:18:27 +0800

    Update AlphabetIndexBar.h

--- my_project/AlphabetIndexBar.h
+++ my_project/AlphabetIndexBar.h
@@ -1,4 +1,3 @@
-
 #ifndef ALPHABETINDEXBAR_H
 #define ALPHABETINDEXBAR_H
 
@@ -17,16 +16,16 @@ public:
      QList<QChar> getIndexLetters() const { return indexLetters; }
 
 signals:
-    void letterClicked(QChar letter);
+    void letterClicked(QChar letter);//返回当前被点击的字母
 
 private:
-    QMap<QChar, QPushButton*> letterButtons;
+    QMap<QChar, QPushButton*> letterButtons;//利用map将按键和字母相对应
     QList<QChar> indexLetters = {'#',
         'A','B','C','D','E','F','G','H','I','J','K','L','M',
-        'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};//存储字母
 
     QChar currentHighlighted = QChar(); // 当前高亮字母
-    void setupLayout();
+    void setupLayout();//垂直布局
 };
 
 #endif // ALPHABETINDEXBAR_H
