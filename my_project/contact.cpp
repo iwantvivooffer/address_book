48b49ad02361204abbd63a2c1f0c9d588fc53112
yinyinyin <yinxi251221@gmail.com>
2025-07-23 15:18:57 +0800

    Update contact.cpp

--- my_project/contact.cpp
+++ my_project/contact.cpp
@@ -15,7 +15,7 @@ QString contact::getgroup() const {
     return group;
 }
 QString contact::getname() const {
-    return name.trimmed().replace("姓名：", "");
+    return name.trimmed().replace("姓名：", "");//trimmed可以去除字符串前后两端的空白字符
 }
 QString contact::getnumber() const {
     return number;
@@ -38,6 +38,7 @@ void contact::setnumber(const QString &number) {
 //保存到JSON文件里面
 QJsonObject contact::toJson() const {
     QJsonObject obj;
+    //将各个属性作为一个键值放入obj
     obj["name"] = name;
     obj["number"] = number;
     obj["group"] = group;
@@ -47,6 +48,7 @@ QJsonObject contact::toJson() const {
 //从JSON文件里面读取
 contact contact::fromJson(const QJsonObject &obj) {
     contact c;
+    //从对应的键值中读取数据
     c.setname(obj["name"].toString());
     c.setnumber(obj["number"].toString());
     c.setgroup(obj["group"].toString());
