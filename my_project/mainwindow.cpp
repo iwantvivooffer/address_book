569e6e0b13c09ba9d9dc2397e6e0cbf389df6bd2
yinyinyin <yinxi251221@gmail.com>
2025-07-23 09:22:03 +0800

    Update mainwindow.cpp

--- my_project/mainwindow.cpp
+++ my_project/mainwindow.cpp
@@ -20,7 +20,7 @@ MainWindow::MainWindow(QWidget *parent) :
     // 初始化UI
     setWindowTitle(isChinese ? "小型通讯录" : "Small Address Book");
     resize(500, 750);
-    setWindowIcon(QIcon("://res/picture/icon.jpg"));
+    setWindowIcon(QIcon("://res/picture/icon.png"));
 
     // 从文件加载联系人数据
     m_contact.loadFromJson(path);
@@ -55,9 +55,9 @@ MainWindow::MainWindow(QWidget *parent) :
             border: none;
         }
         QComboBox::down-arrow {
-            image: url(:/res/picture/down-arrow.png); /* 可选: 自定义箭头图标 */
-            width: 12px;
-            height: 12px;
+            image: url(:/res/picture/down-arrow.png);
+            width: 20px;
+            height: 20px;
         }
         QComboBox QAbstractItemView {
             background-color: rgba(255, 255, 255, 230);
@@ -85,19 +85,12 @@ MainWindow::MainWindow(QWidget *parent) :
     langButton->setIcon(langIcon);
     langButton->setIconSize(QSize(30, 30));
     langButton->setFlat(true);
-    // 构建搜索布局
-    searchLayout->addWidget(searchBtn);
-    searchLayout->addWidget(searchCombo);
-    searchLayout->addWidget(searchEdit);
-    searchLayout->addStretch();
-    searchLayout->addWidget(langButton);
-    searchLayout->addWidget(addButton);
 
     // 联系人列表
     contactlist = new QListWidget;
     contactlist->setStyleSheet(R"(
         QListWidget {
-            background-color: rgba(255, 255, 255, 120);
+            background-color: rgba(255, 255, 255,120 );
             border-radius: 12px;
             border: 1px solid rgba(255, 255, 255, 80);
             padding: 6px;
@@ -149,6 +142,37 @@ MainWindow::MainWindow(QWidget *parent) :
 
 
     contactlist->setSpacing(8);//设置间距
+
+    // 光暗切换按钮
+    modeToggleButton = new QPushButton(this);
+    modeToggleButton->setFixedSize(30, 30);
+    QIcon modeIcon(":/res/picture/光暗转换.png");
+    modeToggleButton->setIcon(modeIcon);
+    modeToggleButton->setIconSize(QSize(30, 30));
+    modeToggleButton->setFlat(true);
+    modeToggleButton->setStyleSheet(R"(
+        QPushButton {
+            background-color: transparent;
+            border: none;
+        }
+        QPushButton:hover {
+            background-color: rgba(0, 0, 0, 0.05);  /* 鼠标悬停轻微高亮 */
+            border-radius: 6px;
+        }
+    )");
+
+
+
+    // 构建搜索布局
+    searchLayout->addWidget(searchBtn);
+    searchLayout->addWidget(searchCombo);
+    searchLayout->addWidget(searchEdit);
+    searchLayout->addStretch();
+    searchLayout->addWidget(modeToggleButton);
+    searchLayout->addWidget(langButton);
+    searchLayout->addWidget(addButton);
+
+
     // 主布局
     layout->setContentsMargins(20, 20, 20, 20);//设置边距
     layout->setSpacing(15);
@@ -165,28 +189,11 @@ MainWindow::MainWindow(QWidget *parent) :
         color: black;
     )";
     searchEdit->setStyleSheet(blurStyle);
-    
-    
+
+
     // 初始化UI语言
-    updateUILanguage(); 
-    
-    // 光暗切换按钮
-    modeToggleButton = new QPushButton(this);
-    modeToggleButton->setFixedSize(40, 40); 
-    QIcon modeIcon(":/res/picture/光暗转换.png");
-    modeToggleButton->setIcon(modeIcon);
-    modeToggleButton->setIconSize(QSize(35, 35)); 
-    modeToggleButton->setFlat(true); 
-    modeToggleButton->setStyleSheet(R"(
-        QPushButton {
-            background-color: transparent;
-            border: none;
-        }
-        QPushButton:hover {
-            background-color: rgba(0, 0, 0, 0.05);  /* 鼠标悬停轻微高亮 */
-            border-radius: 6px;
-        }
-    )");
+    updateUILanguage();
+
 
 
     // 连接信号槽
@@ -207,18 +214,16 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(infoPage, &InformationPage::deleteContact, this, &MainWindow::onDeleteContact);
     // 应用初始样式（亮色模式）
     applyLightMode();
-    
+
 }
 
-// 刷新联系人列表
 void MainWindow::refreshContactList() {
-
     contactlist->clear();
     m_contact.sortContactsByName();
 
     QFont itemFont("方正舒体", 20);
     QList<contact> all = m_contact.getcontacts();
-    
+
     for(auto &c : all) {
         //用\n来实现换行显示
         QString text = isChinese ? QString("姓名：%1\n电话：%2").arg(c.getname()).arg(c.getnumber())
@@ -267,14 +272,13 @@ void MainWindow::refreshContactListFiltered(const QString &filter, const QString
 void MainWindow::paintEvent(QPaintEvent *)
 {
     QPainter painter(this);
-    QPixmap bg(":/res/picture/bk3.webp");
+    QString path=QString(":/res/picture/bk3.jpg");
+    QPixmap bg(path);
     if (!bg.isNull()) {
-        painter.drawPixmap(this->rect(), bg); //背景，自动填充
+        painter.drawPixmap(this->rect(), bg);//背景
     }
 }
 
-
-// 保存联系人修改
 void MainWindow::onSaveContact(const contact original, const contact modified) {
     if(original.getname().isEmpty()) {
         if(modified.getname().isEmpty() || modified.getnumber().isEmpty()) {
@@ -308,8 +312,6 @@ void MainWindow::onSaveContact(const contact original, const contact modified) {
                             isChinese ? "联系人信息已更新" : "Contact information updated");
 }
 
-
-// 删除联系人
 void MainWindow::onDeleteContact(QString name)
 {
     if (m_contact.deletecontact(name)) {
@@ -323,12 +325,10 @@ void MainWindow::onDeleteContact(QString name)
     }
 }
 
-//搜索
 void MainWindow::onSearchContact() {
     QString filter = searchEdit->text();
     QString field = searchCombo->currentText();
-
-    refreshContactListFiltered(filter, field);//调用刷新界面来实现“搜索”
+    refreshContactListFiltered(filter, field);//调用刷新界面的函数来实现“搜索”
 }
 
 void MainWindow::onContactItemClicked(QListWidgetItem *item)
@@ -365,24 +365,22 @@ void MainWindow::onContactItemClicked(QListWidgetItem *item)
     QMessageBox::warning(this,
                        isChinese ? "查找失败" : "Search Failed",
                        isChinese ? "未找到该联系人信息！请检查姓名是否完全一致。"
-                        : "Contact not found! Please check if the name is exactly the same.");
+                                : "Contact not found! Please check if the name is exactly the same.");
 }
 
-// 处理添加按钮点击
 void MainWindow::onAddButtonClicked()
 {
-    // 创建空联系人
     contact newContact;
     infoPage->setLanguageState(isChinese);
     infoPage->showContactDetails(newContact);
     infoPage->slideIn();
 }
 
-// 处理信息页面返回按钮点击事件
 void MainWindow::onInfoPageBackClicked()
 {
     infoPage->slideOut();
 }
+
 // 光暗模式切换
 void MainWindow::toggleDarkMode()
 {
@@ -503,7 +501,6 @@ void MainWindow::updateUILanguage()
     // 刷新列表显示
     refreshContactList();
 }
-
 
 MainWindow::~MainWindow()
 {
